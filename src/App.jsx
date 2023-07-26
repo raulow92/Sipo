5627af160f01f1eba82c7d0fb5ef7cbf417baa12
Raul Low <raulou92@gmail.com>
2023-07-26 17:06:21 -0400

    adding LogInMain

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import PublicHeader from "@/components/header/PublicHeader";
 import WelcomeMain from "@/components/main/WelcomeMain";
 import SignUpMain from "@/components/main/SignUpMain";
+import LogInMain from "@/components/main/LogInMain";
 
 const App = () => {
     return (
         <>
           <PublicHeader />
+          <LogInMain />
           <SignUpMain />
           {/* <WelcomeMain /> */}
         </>
--- src/components/main/LogInMain.jsx
+++ src/components/main/LogInMain.jsx
@@ -1,6 +1,6 @@
 import LogoSipo from "@/assets/img/logo.svg";
 
-const SignUpMain = () => {
+const LogInMain = () => {
     return (
         <main className="container mx-auto grid grid-cols-1 xl:grid-cols-2 md:items-center px-6 py-6 md:px-24 md:py-20">
             <section className="hidden xl:flex flex-col mx-auto md:pl-12">
@@ -12,11 +12,12 @@ const SignUpMain = () => {
             <section className="xl:mx-full xl:ml-12 2xl:ml-24 2xl:mr-12">
                 <form className="bg-white p-8 w-auto text-lg rounded-xl md:drop-shadow-md">
                     <div className="flex flex-col">
-                        <input type="text" id="name" placeholder="Nombre" className="border border-gray-300 rounded-xl p-4 pl-6" />
-                        <input type="text" id="last-name" placeholder="Apellidos" className="border border-gray-300 rounded-xl p-4 pl-6 mt-5" />
-                        <input type="email" id="email" placeholder="Correo electrónico" className="border border-gray-300 rounded-xl p-4 pl-6 mt-5" />
+                        <input type="email" id="email" placeholder="Correo electrónico" className="border border-gray-300 rounded-xl p-4 pl-6" />
                         <input type="password" id="password" placeholder="Contraseña nueva" className="border border-gray-300 rounded-xl p-4 pl-6 mt-5" />
-                        <input type="submit" value="Regístrate" className="hover:cursor-pointer bg-green-400 font-medium rounded-xl text-white p-4 mt-5"></input>
+                        <input type="submit" value="Iniciar sesión" className="hover:cursor-pointer bg-sky-400 font-medium rounded-xl text-white p-4 mt-5"></input>
+                        <p className="text-center text-sky-400 font-medium text-base mt-5 hover:cursor-pointer">¿Has olvidado la contraseña?</p>
+                        <div className="border-solid border-t-2 border-gray-300 mt-5"></div>
+                        <button className="bg-green-400 self-center px-6 font-medium rounded-xl text-white p-4 mt-8">Crear cuenta nueva</button>
                     </div>
                 </form>
             </section>
@@ -24,4 +25,4 @@ const SignUpMain = () => {
     );
 };
 
-export default SignUpMain;
+export default LogInMain;
