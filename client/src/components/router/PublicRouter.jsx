87abed8a0e0eefe64d0aea554d6657cdc55eec2d
Raul Low <raulou92@gmail.com>
2023-08-03 18:28:04 -0400

    fixing public router error

--- client/src/components/router/PublicRouter.jsx
+++ client/src/components/router/PublicRouter.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import WelcomeMain from "@/components/views/WelcomeMain";
 import SignUpMain from "@/components/views/SignUpMain";
 import LogInMain from "@/components/views/LogInMain";
+import Tienda from "@/components/views/Tienda";
 
 const PublicRouter = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<WelcomeMain />} />
+                <Route path="/tienda" element={<Tienda />} />
                 <Route path="/signup" element={<SignUpMain />} />
                 <Route path="/login" element={<LogInMain />} />
             </Routes>
