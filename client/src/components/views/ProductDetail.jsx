bc08657b592128ff6e86a235b77e471ba119df86
Raul Low <raulou92@gmail.com>
2023-08-09 16:39:04 -0400

    Added ProductDetail favorites function

--- client/src/components/views/ProductDetail.jsx
+++ client/src/components/views/ProductDetail.jsx
@@ -16,7 +16,9 @@ const ProductDetail = () => {
     const [product, setProduct] = useState([]);
     const { id } = useParams();
     const navigate = useNavigate();
-    const { usuario } = useContext(Context);
+    const { usuario, setUsuario: setUsuarioGlobal } = useContext(Context);
+    const [heartFill, setHeartFill] = useState("none");
+    const [heartStroke, setHeartStroke] = useState("white");
 
     const handleBuy = async (e) => {
         e.preventDefault();
@@ -37,18 +39,89 @@ const ProductDetail = () => {
         try {
             const endpoint = `product/${id}`;
             const response = await axios.get(url + endpoint);
-            const productData = response.data
+            const productData = response.data;
             setProduct(productData);
-            console.log(productData);
         } catch (error) {
             console.log(error);
         }
     };
 
+    const handleHeartFill = () => {
+        const findFav = (producto) => {
+            return producto.product_id == id;
+        };
+        if (usuario.favorites.find(findFav)) {
+            setHeartFill("red");
+            setHeartStroke("red");
+        } else {
+            setHeartFill("none");
+            setHeartStroke("white");
+        }
+    };
+
+    const getUserData = async () => {
+        const endpoint = "users";
+        const token = localStorage.getItem("token");
+        try {
+            const { data } = await axios.get(url + endpoint, {
+                headers: { Authorization: "Bearer " + token },
+            });
+            const favEndpoint = `user/${data.user_id}/favorites`;
+            const { data: favorites } = await axios.get(url + favEndpoint);
+            const result = {
+                data,
+                favorites,
+            };
+            setUsuarioGlobal(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleFavorite = async (id) => {
+        const endpoint = "favorites";
+        const userEndpoint = `/${usuario.data.user_id}/${id}`;
+        try {
+            const { data: userFavorites } = await axios.get(
+                url + endpoint + userEndpoint
+            );
+            if (!userFavorites) {
+                const { data: response } = await axios.post(url + endpoint, {
+                    user_id: usuario.data.user_id,
+                    product_id: id,
+                });
+                console.log(response);
+                getUserData();
+            } else {
+                try {
+                    const { data: response } = await axios.delete(
+                        url + endpoint + userEndpoint
+                    );
+                    console.log(response);
+                    getUserData();
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleHeartClick = (e) => {
+        e.stopPropagation();
+        setHeartFill(heartFill === "none" ? "red" : "none");
+        setHeartStroke(heartStroke === "white" ? "red" : "white");
+        handleFavorite(id);
+    };
+
     useEffect(() => {
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
         getProduct();
+        getUserData();
+        handleHeartFill();
     }, []);
+
 
     if (!product) return <p>Loading...</p>;
 
@@ -66,7 +139,7 @@ const ProductDetail = () => {
                     />
                 </div>
                 <div>
-                    <SellerCard id={id}/>
+                    <SellerCard id={id} />
                     <div className="bg-white mt-6 p-6 rounded-lg shadow-md">
                         <h2 className="text-xl font-medium mb-1">
                             Detalles del producto
@@ -86,9 +159,10 @@ const ProductDetail = () => {
                             Comprar
                         </button>
                         <HeartIcon
-                            filled="none"
-                            stroke="white"
-                            className="w-10 mx-auto"
+                            filled={heartFill}
+                            stroke={heartStroke}
+                            onClick={handleHeartClick}
+                            className="w-10 mx-auto cursor-pointer"
                         />
                     </div>
                 </div>
